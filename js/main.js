ffa3d1862ed456625434545c63bef445e249209d
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2025-05-25 11:22:00 +0100

    Version Release 1.0.4
    Small content update that includes new information about upcoming project development.

--- js/main.js
+++ js/main.js
@@ -12,6 +12,6 @@ jQuery(() => {
     $(".getYear").text(new Date().getUTCFullYear())
 
     // getVersion 
-    $(".getVersion").html("1.0.3 Alpha &bull; <a href='/version-changelog.html'>Changelog</a>")
+    $(".getVersion").html("1.0.4 Alpha &bull; <a href='/version-changelog.html'>Changelog</a>")
 
 })
